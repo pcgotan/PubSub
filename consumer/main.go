9039a5d529b26149cb85bb500d92d3bc9789dc91
pcgotan <chouhan@iitk.ac.in>
2020-05-24 13:56:26 +0530

    files modified

---
+++ consumer/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"consumer/internal/app/handler"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if os.Args[1] == "mail" {
+		handler.StartConsumerMail()
+
+	} else if os.Args[1] == "sms" {
+		handler.StartConsumerSms()
+	} else {
+		fmt.Println("Command not found")
+	}
+}
